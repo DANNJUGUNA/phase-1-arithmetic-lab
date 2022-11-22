14d2db9cb1b47eaf4bdf9cbbf1fa101173c34c6f
DANNJUGUNA <ndan30071@gmail.com>
2022-11-22 18:32:50 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let num3=19;
+let num4=5;
+let max;
+let multiply=num1*num2;
+let mod=num3%num4;
+ max=Math.max(12,13,20)
+
+
+let random;
+random= (Math.floor(Math.random()*10)+1);
